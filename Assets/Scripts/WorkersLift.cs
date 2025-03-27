a7ab5cc4fcac58c71001913b266e4d7b6088b14d
Jake <j.mcmillan8@rgu.ac.uk>
2025-03-27 16:47:43 +0000

    Grab Objects

--- Assets/Scripts/WorkersLift.cs
+++ Assets/Scripts/WorkersLift.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WorkersLift : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class WorkersLift : MonoBehaviour {
+    public Rigidbody platform;
+    
+    void Start(){
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    
+    void Update(){
+        platform.transform.position += Vector3.up * 0.2f * Time.deltaTime;
+  
     }
 }
