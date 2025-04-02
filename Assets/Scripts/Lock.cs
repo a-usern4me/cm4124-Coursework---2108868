f1aa5d49453befda3804dbaae2e9bb721e4a5d04
Jake <j.mcmillan8@rgu.ac.uk>
2025-04-02 14:52:37 +0100

    Refuelling Mechanic

---
+++ Assets/Scripts/Lock.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour {
+    [SerializeField] GameObject deposit;
+    [SerializeField] GameObject cannister;
+    [SerializeField] GameObject blowtorch;
+    private bool empty;
+  
+    void Start(){
+        empty = true;
+        blowtorch.GetComponent<Rigidbody>().isKinematic = true;
+        blowtorch.GetComponent<BoxCollider>().enabled = false;
+        
+    }
+
+    void Update(){
+        
+    }
+
+    private void OnTriggerEnter(Collider collider){
+        if (collider.tag == "Fuel Cannister" && empty == true){
+            Refuel();
+
+        }
+
+    }
+
+    private void Refuel(){
+        blowtorch.GetComponent<Rigidbody>().isKinematic = false;
+        blowtorch.GetComponent<BoxCollider>().enabled = true;
+        Debug.Log("Activated!");
+
+    }
+}
